163fcec379c93e4b6ce1368f7c04fa020eaf1634
Maysam Torabi <kherad@gmail.com>
2026-03-01 02:24:58 +0100

    adding slash to base

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/app.ehsana.com/',
   server: {
     port: parseInt(process.env.PORT ?? '5173'),
-  },
-  base: '/app.ehsana.com',
+  }
 })
